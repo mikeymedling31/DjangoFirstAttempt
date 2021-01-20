2fd930d6bbde82602d831abb5573761f8f22894f
Michael <mikeymedling31@gmail.com>
2021-01-20 15:53:02 -0500

    Part 3 Django

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'widget_tweaks',
     'boards',
 ]
 
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
